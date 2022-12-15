1dcf97a55647e8c7e87776d0bf3a7a42fe12a554
dilancelik18 <83242771+dilancelik18@users.noreply.github.com>
2022-12-15 18:05:58 +0300

    Add files via upload

---
+++ Models/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
+
+namespace RentaCar.Models
+{
+    public class AppDBContext:DbContext
+    {
+        private AppDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Arac> Aracs { get; set; }
+    }
+}
---
+++ Models/Arac.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RentaCar.Models
+{
+    [Table("Aracs")]
+    public class Arac
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public int Price { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
