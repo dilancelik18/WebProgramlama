058912379f964f2db458c5a92f09f24110a58768
dilancelik18 <83242771+dilancelik18@users.noreply.github.com>
2022-12-23 02:12:48 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentCar.Models;
+using System.Diagnostics;
+
+namespace RentCar.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+     
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+    }
+}
